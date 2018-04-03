6ac1575d24b910c48d2917e2cf704b17540dac42
Chetan Mittal <mittalchetan98@gmail.com>
2018-04-03 10:02:50 +0530

    added local defs

--- AbstractMachines.ml
+++ AbstractMachines.ml
@@ -4,6 +4,7 @@ exception Not_found
 type exp = Var of string
 | Lambda of exp*exp
 | Call of exp*exp
+| Unit
 | T
 | F
 | Const of int
@@ -14,7 +15,15 @@ type exp = Var of string
 | Lst of exp*exp
 | Gre of exp*exp
 | Lte of exp*exp
+| Tup of (exp list)
+| Proj of int*(exp)
 | IfThenElse of exp*exp*exp
+| Let of exp*exp 					(* first exp can be Def/SeqDefs/ParallelDefs/LocalFirstInSecondDef *)
+| Def of exp*exp 					(* first exp to be Var *)
+| SeqDefs of exp*exp 				(* both exps to be defs *)
+| ParallelDefs of exp*exp 			(* both exps to be defs *)
+
+(* And,Or,Not,Defs *)
 
 let rec lookup t x = match t with
 | [] -> raise Not_found
@@ -25,6 +34,8 @@ type answerK = UnitK
 | BoolTK
 | BoolFK
 | IntK of int
+| AListK of answerK list
+| TableExtensionK of exp*(answerK list)
 | VClosureK of ((exp * exp) list) * exp
 
 let rec eval_call_by_name e = match e with
@@ -34,6 +45,7 @@ let rec eval_call_by_name e = match e with
 	| VClosureK (t1,Lambda (Var x,e1)) -> eval_call_by_name ((Var x,b)::t1,e1)
 	| _ -> raise Error
 	)
+| (_,Unit) -> UnitK
 | (_,T) -> BoolTK
 | (_,F) -> BoolFK
 | (_,Const a) -> IntK a
@@ -65,11 +77,14 @@ let rec eval_call_by_name e = match e with
 	| (IntK a1, IntK a2) -> if a1<=a2 then BoolTK else BoolFK
 	| (_,_) -> raise Error
 	)
+| (t,Tup a) -> AListK (List.map (eval_call_by_name) (List.map ((fun x -> (t,x))) a))
+| (t,Proj (a,Tup b)) -> eval_call_by_name (t,(List.nth b a))
 | (t,IfThenElse (a,b,c)) -> (match (eval_call_by_name (t,a)) with
 	| BoolTK -> eval_call_by_name (t,b)
 	| BoolFK -> eval_call_by_name (t,c)
 	| _ -> raise Error
 	)
+| _ -> raise Error
 
 (* 
 eg : let a = Call (Lambda (Add (Var "x",Const 1)), Const 2);;
@@ -83,6 +98,7 @@ type opcode =
 | Oclosure of exp*(opcode list)
 | Oret
 | Oapply
+| Ounit
 | Otrue
 | Ofalse
 | Oint of int
@@ -93,12 +109,20 @@ type opcode =
 | Olst
 | Ogre
 | Olte
+| Otup
+| OtupBeg
+| Oproj
 | OIfThenElse
+| Obind
+| Ounbind
+| Odef of exp
 
-type answer = Unit
+type answer = UnitA
 | BoolT
 | BoolF
 | Int of int
+| AList of answer list
+| TableExtension of (exp * answer) list
 | VClosure of ((exp * answer) list) * exp
 | Closure of exp * ((exp * answer) list) * (opcode list)
 
@@ -109,6 +133,7 @@ let rec eval_call_by_value t e = match e with
 	| VClosure (t1,Lambda (Var x, e1)) -> eval_call_by_value ((Var x, eval_call_by_value t b)::t1) e1
 	| _ -> raise Error
 	)
+| Unit -> UnitA
 | T -> BoolT
 | F -> BoolF
 | Const a -> Int a
@@ -140,16 +165,36 @@ let rec eval_call_by_value t e = match e with
 	| (Int a1, Int a2) -> if (a1 <= a2) then BoolT else BoolF
 	| (_,_) -> raise Error
 	)
+| Tup a -> AList (List.map (eval_call_by_value t) a)
+| Proj (a,Tup b) -> eval_call_by_value t (List.nth b a)
 | IfThenElse (e1,e2,e3) -> (match (eval_call_by_value t e1, eval_call_by_value t e2, eval_call_by_value t e3) with
 	| (BoolT,a1,a2) -> a1
 	| (BoolF,a1,a2) -> a2
 	| (_,_,_) -> raise Error
 	)
+| Let (e1,e2) -> (match (eval_call_by_value t e1) with
+	| TableExtension t1 -> eval_call_by_value (t1@t) e2
+	| _ -> raise Error
+	)
+| Def (Var x,e2) -> TableExtension [(Var x, (eval_call_by_value t e2))]
+| SeqDefs (e1,e2) -> (match (eval_call_by_value t e1) with
+	| TableExtension t1 -> (match (eval_call_by_value (t1@t) e2) with
+		| TableExtension t2 -> TableExtension (t1@t2)
+		| _ -> raise Error
+		)
+	| _ -> raise Error
+	)
+| ParallelDefs (e1,e2) -> (match (eval_call_by_value t e1, eval_call_by_value t e2) with
+	| (TableExtension t1, TableExtension t2) -> TableExtension (t1@t2)
+	| (_,_) -> raise Error
+	)
+| _ -> raise Error
 
 let rec compile e = match e with
 | Var a -> [Ovar a]
 | Lambda (a,b) -> [Oclosure (a,((compile b)@[Oret]))]
 | Call (a,b) -> (compile a)@(compile b)@[Oapply]
+| Unit -> [Ounit]
 | T -> [Otrue]
 | F -> [Ofalse]
 | Const a -> [Oint a]
@@ -160,7 +205,15 @@ let rec compile e = match e with
 | Lst (e1,e2) ->  (compile e1)@(compile e2)@[Olst]
 | Gre (e1,e2) ->  (compile e1)@(compile e2)@[Ogre]
 | Lte (e1,e2) ->  (compile e1)@(compile e2)@[Olte]
+| Tup a ->  (match (a) with
+    | x::[] -> (compile x)@[OtupBeg]
+    | x::xs -> (compile (Tup xs))@(compile x)@[Otup]
+	| _ -> raise Error )
+| Proj (a,Tup b) -> [Oint a]@(compile (Tup b))@[Oproj]
 | IfThenElse (e1,e2,e3) -> (compile e1)@(compile e2)@compile(e3)@[OIfThenElse]
+| Let (e1,e2) -> (compile e1)@[Obind]@(compile e2)@[Ounbind]
+| Def (Var x, e2) -> (compile e2)@[Odef (Var x)]
+| _ -> raise Error
 
 let rec execute s t c d = match (s,c) with
 | (s0::s1, []) -> s0
@@ -171,6 +224,7 @@ let rec execute s t c d = match (s,c) with
 	| (s1,t1,c1)::d1 -> execute (s0::s1) t1 c1 d1
 	| _ -> raise Error
 	)
+| (_,(Ounit)::c1) -> execute (UnitA::s) t c1 d
 | (_,(Otrue)::c1) -> execute (BoolT::s) t c1 d
 | (_,(Ofalse)::c1) -> execute (BoolF::s) t c1 d
 | (_, (Oint b)::c1) -> execute ((Int b)::s) t c1 d
@@ -181,8 +235,17 @@ let rec execute s t c d = match (s,c) with
 | ((Int a0)::(Int a1)::s2, (Olst)::c1) -> if (a1<a0) then execute (BoolT::s2) t c1 d else execute (BoolF::s2) t c1 d
 | ((Int a0)::(Int a1)::s2, (Ogre)::c1) -> if (a1>=a0) then execute (BoolT::s2) t c1 d else execute (BoolF::s2) t c1 d
 | ((Int a0)::(Int a1)::s2, (Olte)::c1) -> if (a1<=a0) then execute (BoolT::s2) t c1 d else execute (BoolF::s2) t c1 d
+| (a::(AList b)::s1, Otup::c1) -> execute ((AList (a::b))::s1) t c1 d
+| (a::s1, OtupBeg::c1) -> execute ((AList [a])::s1) t c1 d
+| ((AList b)::(Int a)::s1,Oproj::c1) -> execute ((List.nth b a)::s1) t c1 d
 | (a0::a1::BoolT::s1, (OIfThenElse)::c1) -> execute (a1::s1) t c1 d
 | (a0::a1::BoolF::s1, (OIfThenElse)::c1) -> execute (a0::s1) t c1 d
+| ((TableExtension t1)::s1, (Obind)::c1) -> execute s1 (t1@t) c1 ((s1,t,c1)::d)
+| (_,(Ounbind)::c1) -> (match d with
+	| (s1,t1,c2)::d1 -> execute s t1 c1 d1
+	| _ -> raise Error
+	)
+| (a0::s1, (Odef (Var x))::c1) -> execute ((TableExtension [(Var x,a0)])::s1) t c1 d
 | (_,_) -> raise Error
 
 (* 
